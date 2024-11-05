bfda097a93952a0e29847d3924644d6e4bd8983f
Antoine SCHUH <aschuh@pictet-technologies.com>
2024-11-05 11:34:11 +0100

    Command with channel param

--- commands.js
+++ commands.js
@@ -2,14 +2,22 @@ import 'dotenv/config';
 import { InstallGlobalCommands } from './utils.js';
 
 // Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
+const PRESENCE_COMMAND = {
+  name: 'presence',
+  description: 'Command to generate a presence report',
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 1, 2],
+  options: [
+    {
+      name: 'channel',
+      description: 'The channel to generate the presence report for',
+      type: 7, // Type 7 represents a channel option
+      required: false
+    },
+  ]
 };
 
-const ALL_COMMANDS = [TEST_COMMAND];
+const ALL_COMMANDS = [PRESENCE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
